fa6480f83c58b10f136d7990e7de68e5f344eb14
vanshika703 <vanshikabhojnagarwala@gmail.com>
2020-05-13 19:16:33 +0530

    some changes

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 const {OAuth2Client} = require('google-auth-library')
-const client = new OAuth2Client(523384873779-75adpe7lmrvnue4d6ngr5dme4i0gn4ck.apps.googleusercontent.com)
+const client = new OAuth2Client("523384873779-e29ttamvfnbfkhb650ppufoas5qmr328.apps.googleusercontent.com")
 
 app.use(express.urlencoded({extended:false}))
 app.use(express.json())
@@ -15,12 +15,12 @@ app.post('/gsignin', (req,res) => {
     async function verify() {
         const ticket = await client.verifyIdToken({
             idToken : req.body.idtoken,
-            audience : 523384873779-75adpe7lmrvnue4d6ngr5dme4i0gn4ck.apps.googleusercontent.com
+            audience : "523384873779-e29ttamvfnbfkhb650ppufoas5qmr328.apps.googleusercontent.com"
     })
     const payload = ticket.getPayload()
     const userid = payload['sub']
     }
-    res.send("hi")
+    res.send()
     verify().catch(console.error)
 })
 
